ea9e0f32b7e8426b9078e9f7d4c1dd8b29fbae42
rian33svan <96176662+RianErick@users.noreply.github.com>
2023-01-26 16:13:09 -0300

    Criaçao Do Metodo Create.save

--- src/main/java/com/crud/exercicio/alunos_api/controller/AlunoController.java
+++ src/main/java/com/crud/exercicio/alunos_api/controller/AlunoController.java
@@ -2,13 +2,14 @@ package com.crud.exercicio.alunos_api.controller;
 import com.crud.exercicio.alunos_api.model.Aluno;
 import com.crud.exercicio.alunos_api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
 import java.util.List;
 
-
-
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
@@ -18,11 +19,16 @@ public class AlunoController {
     public String status(){
         return "Online";
     }
-
-    @GetMapping("/alunos")
+    @GetMapping("/listar")
     public List <Aluno> listarAlunos(){
         return alunoRepository.findAll();
     }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Aluno adicionar (@RequestBody Aluno aluno){
+        return alunoRepository.save(aluno);
+    }
+
 
 
 }
--- src/main/java/com/crud/exercicio/alunos_api/repository/AlunoRepository.java
+++ src/main/java/com/crud/exercicio/alunos_api/repository/AlunoRepository.java
@@ -2,9 +2,11 @@ package com.crud.exercicio.alunos_api.repository;
 
 import com.crud.exercicio.alunos_api.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AlunoRepository extends JpaRepository<Aluno,Long> {
 
      List <Aluno> findByNome(String nome);
