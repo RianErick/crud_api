de1f04b6270d503b2ca019593a76419e420d7c35
rian33svan <96176662+RianErick@users.noreply.github.com>
2023-01-26 18:09:46 -0300

    upadade / delete add

--- src/main/java/com/crud/exercicio/alunos_api/controller/AlunoController.java
+++ src/main/java/com/crud/exercicio/alunos_api/controller/AlunoController.java
@@ -3,9 +3,8 @@ import com.crud.exercicio.alunos_api.model.Aluno;
 import com.crud.exercicio.alunos_api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.List;
 
 @RestController
@@ -15,18 +14,32 @@ public class AlunoController {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    @GetMapping("/status")
-    public String status(){
-        return "Online";
-    }
-    @GetMapping("/listar")
-    public List <Aluno> listarAlunos(){
-        return alunoRepository.findAll();
-    }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Aluno adicionar (@RequestBody Aluno aluno){
+    public Aluno create (@RequestBody Aluno aluno){
         return alunoRepository.save(aluno);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity <Aluno> update (@PathVariable Long id , @RequestBody Aluno aluno){
+        if(!alunoRepository.existsById(id)){
+            return  ResponseEntity.notFound().build();
+        }
+        aluno.setId(id);
+        aluno = alunoRepository.save(aluno);
+        return ResponseEntity.ok(aluno);
+    }
+    @GetMapping("/listar")
+    public List <Aluno> read(){
+        return alunoRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> delete (@PathVariable Long id){
+         if (!alunoRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        alunoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
